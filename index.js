75911829382dc8281671438b870bca5c8faed752
WinterJackson <winterjacksonwj@gmail.com>
2023-07-01 13:31:06 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -15,10 +15,25 @@ isPalindrome('bob');
 
 /* 
   Add your pseudocode here
+  function isPalindrome(word):
+    create constant reversedWord and turn it into an empty string
+      iterate over the characters of the word starting from the last character 
+      and append each character to reversedWord
+    
+    if reversedWord equals word:
+        return true
+    else:
+        return false
 */
 
 /*
-  Add written explanation of your solution here
+  Add written explanation of your solution here 
+    It initializes a variable called reversedWord to an empty string. 
+    Then, it iterates over the characters of the word starting from the last character (index word.length - 1) 
+    and appends each character to reversedWord.
+    After the loop, it checks if the reversedWord is equal to the original word.
+    If they are equal, it returns true indicating that the word is a palindrome. 
+    Otherwise, it returns false.
 */
 
 // You can run `node index.js` to view these console logs
