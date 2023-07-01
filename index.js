8bddf9aa11501ebbe5fcffbfec0975ebdee2a318
WinterJackson <winterjacksonwj@gmail.com>
2023-07-01 13:22:24 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,6 +1,17 @@
-function isPalindrome(word) {
-  // Write your algorithm here
+function isPalindrome (word){
+  let reversedWord =''
+  for (let i = word.length - 1; i>=0; i--){
+    reversedWord += word[i];
+  }
+  
+  if (reversedWord === word) {
+    return true;
+  } else{
+    return false;
+  }
 }
+
+isPalindrome('bob');
 
 /* 
   Add your pseudocode here
